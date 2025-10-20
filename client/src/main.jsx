ccf8c0b7b44350e4936b5516f8d44845311b166b
Kibugi Maina <bugijay7@gmail.com>
2025-10-21 00:24:13 +0300

    speed insight removal

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { SpeedInsights } from "@vercel/speed-insights/react";
 
 import './index.css'
 import App from './App.jsx'
@@ -9,6 +8,5 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-      <SpeedInsights />
   </StrictMode>,
 )
